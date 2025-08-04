c35e2f44167a68265069460505d3c225f13fc8c0
Vladimir Chizmejyan <harbecox@gmail.com>
2025-08-05 02:16:59 +0400

    fix

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,22 @@ Route::post('validator/{validator}', [ValidateController::class,'validate'])
     ->name('validator');
 
 Route::get('restaurants', function () {
-    return response()->json(Restaurants::query()->pluck('name','id')->toArray());
+    return response()->json(Restaurants::query()
+        ->get()->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'name' => $item->name,
+            ];
+        })->toArray());
 });
 
 Route::get('positions/{id}', function ($id) {
-    return response()->json(Positions::query()->where('restaurants_id', request('id'))->pluck('name','id')->toArray());
+    return response()->json(Positions::query()->where('restaurants_id', request('id'))->get()->map(function ($item) {
+        return [
+            'id' => $item->id,
+            'name' => $item->name,
+        ];
+    })->toArray());
 });
 
 Route::post('post_test',function (\App\Http\Requests\TestRequest $request){
