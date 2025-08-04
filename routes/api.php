1b1b9cac621eff9ad28f77a6a2765ada7629c647
Vladimir Chizmejyan <harbecox@gmail.com>
2025-08-05 02:12:56 +0400

    request test

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
 use App\Http\Controllers\ValidateController;
+use App\Models\Positions;
+use App\Models\Restaurants;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('validator/{validator}', [ValidateController::class,'validate'])
     ->name('validator');
+
+Route::get('restaurants', function () {
+    return response()->json(Restaurants::query()->pluck('name','id')->toArray());
+});
+
+Route::get('positions/{id}', function ($id) {
+    return response()->json(Positions::query()->where('restaurants_id', request('id'))->pluck('name','id')->toArray());
+});
+
+Route::post('post_test',function (\App\Http\Requests\TestRequest $request){
+     return response()->json($request->validated());
+});
